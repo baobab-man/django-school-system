44614fd228496b2212a67cfd2a1de04ef54c46aa
Romeo <baobab_man@kakao.com>
2020-12-30 11:20:23 +0900

    add: create_study_room, test_create_study_room
    add: create_study_room, test_create_study_room

--- time_tables/tests/test_study_rooms.py
+++ time_tables/tests/test_study_rooms.py
@@ -2,9 +2,17 @@ from django.test import TestCase
 from time_tables.models import StudyRoom
 
 
-def create_study_rooms():
+def create_study_room():
+    study_room_name = 'study_room'
+    study_room = StudyRoom.objects.create(
+        name=study_room_name,
+    )
+    return study_room
+
+
+def create_study_rooms(start, end):
     study_rooms = []
-    for i in range(1, 3):
+    for i in range(start, end + 1):
         study_room_name = 'study_room' + str(i)
         study_room = StudyRoom(
             name=study_room_name,
@@ -15,8 +23,12 @@ def create_study_rooms():
 
 class TestStudyRooms(TestCase):
 
+    def test_create_study_room(self):
+        study_room = create_study_room()
+        self.assertNotEqual(study_room, None)
+
     def test_create_study_rooms(self):
-        create_study_rooms()
+        create_study_rooms(1, 2)
         study_rooms = StudyRoom.objects.all()
-        for study_room in study_rooms:
-            print(study_room)
+        self.assertEqual(study_rooms.count(), 2)
+
