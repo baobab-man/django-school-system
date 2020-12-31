05e9a00088af87fc26aa4afa37f7e6b221898f48
Romeo <baobab_man@kakao.com>
2020-12-31 17:31:22 +0900

    add: TimeTable의 name필드 추가
    add: TimeTable의 name필드 추가

--- time_tables/models/time_table.py
+++ time_tables/models/time_table.py
@@ -2,10 +2,14 @@ from django.db import models
 
 
 class TimeTable(models.Model):
+    name = models.CharField(
+        max_length=30,
+        help_text='시간표명',
+        null=True,
+    )
     study_room = models.OneToOneField(
         'time_tables.StudyRoom',
         on_delete=models.SET_NULL,
         null=True,
-        help_text='반'
+        help_text='반',
     )
-
