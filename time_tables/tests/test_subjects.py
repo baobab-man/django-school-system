1c22b4918e01eabd62edf1b808b09d0683817c6c
Romeo <baobab_man@kakao.com>
2021-01-04 16:17:18 +0900

    add: subject_name -> name으로 수정, time_table_record 파라미터 삭제
    add: subject_name -> name으로 수정, time_table_record 파라미터 삭제

--- time_tables/tests/test_subjects.py
+++ time_tables/tests/test_subjects.py
@@ -2,25 +2,23 @@ from django.test import TestCase
 from time_tables.models import Subject
 
 
-def create_subject(time_table_record=None, teacher=None):
+def create_subject(teacher=None):
     subject_name = Subject.SubjectName.choices[0]
     subject = Subject.objects.create(
-        subject_name=subject_name,
-        time_table_record=time_table_record,
+        name=subject_name,
     )
     if teacher:
         subject.teachers.add(teacher)
     return subject
 
 
-def create_subjects(time_table_record=None, teacher=None):
+def create_subjects(n, teacher=None):
     subjects = []
     subject_choices = Subject.SubjectName.choices
-    subject_names = [choice[0] for choice in subject_choices]
+    subject_names = n * [choice[0] for choice in subject_choices]
     for subject_name in subject_names:
         subject = Subject(
-            subject_name=subject_name,
-            time_table_record=time_table_record,
+            name=subject_name,
         )
         if teacher:
             subject.teachers.add(teacher)
@@ -37,7 +35,7 @@ class TestSubjects(TestCase):
     def test_create_subjects(self):
         before_subject_count = Subject.objects.all().count()
         new_subject_count = len(Subject.SubjectName.choices)
-        create_subjects()
+        create_subjects(1)
         subjects = Subject.objects.all()
         expected_subject_count = before_subject_count + new_subject_count
         self.assertEqual(subjects.count(), expected_subject_count)
