806eda01564e59d5b8e947e9322b35361decb076
Romeo <baobab_man@kakao.com>
2021-01-06 10:51:51 +0900

    add: Docstring Convention 적용
    add: Docstring Convention 적용

--- time_tables/models/student.py
+++ time_tables/models/student.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Student(models.Model):
+    """
+    student 모델
+    :field name: Char, 학생이름
+    :field study_room: FK, 반
+    """
     name = models.CharField(
         max_length=30,
         help_text='학생이름'
